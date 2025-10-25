39730d89cc42728dda05f58ea3546d8dfe8e5020
Shrey-pandey <shrey0408pandey@gmail.com>
2025-10-25 10:28:59 +0530

    Update Task1.java

--- internship_project/Task1.java
+++ internship_project/Task1.java
@@ -1,38 +1,35 @@
-
+import java.util.Random;
 import java.util.Scanner;
-    public class Task1 {
 
-        public static void main(String[] args) {
-            int attempt = 1;
-            int guessedNumberByUser = 0;
-            int expectedNumber = (int) (Math.random() * 99 + 1);
-            Scanner userInput = new Scanner(System.in);
-            System.out.println("Welcome to Number Guessing Game \nYou Will Be Asked To Guess A Number To Win The Game \nYou have Maximum 5 Attempt Limit ");
+public class guessing_game {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int numbertoguess = rand.nextInt(100)+1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess A Number between 1 to 100");
+        int guessed_number=0;
+        int attempts=0;
+        while(guessed_number!=numbertoguess){
+             guessed_number=sc.nextInt();
+            attempts++;
+        if(guessed_number>numbertoguess)
+        {
+            System.out.println("too high");
 
-            do {
-                System.out.print("Enter a guess number between 1 to 100\n");
-                if (userInput.hasNextInt()) {
-                    guessedNumberByUser = userInput.nextInt();
-                    if (guessedNumberByUser == expectedNumber) {
-                        System.out.println("Your Number is right. You Win the Game!");
-                        System.out.println(" It took you " + attempt + " attempts to guess the number.");
-                        break;
-                    } else if (guessedNumberByUser < expectedNumber) {
-                        System.out.println("Your Guess Number is Smaller.");
-                    } else if (guessedNumberByUser > expectedNumber) {
-                        System.out.println("Your Guess Number is Greater.");
-                    }
-                    if (attempt == 5) {
-                        System.out.println("You have exceeded the maximum attempt. Try Again");
-                        break;
-                    }
-                    attempt++;
-                } else {
-                    System.out.println("Guessed number is not a valid integer");
-                    break;
-                }
-            } while (guessedNumberByUser != expectedNumber);
         }
+        else if(guessed_number<numbertoguess)
+        {
+
+            System.out.println("too low");
+
+        }
+        else if(guessed_number==numbertoguess)
+        {
+            System.out.println("bingo! got it right");
+            break;
+        }
+
+        }
+        System.out.println("you guessed it in"+attempts+"attempts");
     }
-
-
+}
